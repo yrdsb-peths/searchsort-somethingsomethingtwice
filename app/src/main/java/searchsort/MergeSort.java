0bda8d9a5b85a664e72c7ab88941fd628b772181
somethingsomethingtwice <167238930+somethingsomethingtwice@users.noreply.github.com>
2024-12-10 21:06:14 -0500

    Mergesort

--- app/src/main/java/searchsort/MergeSort.java
+++ app/src/main/java/searchsort/MergeSort.java
@@ -6,17 +6,36 @@ public class MergeSort extends Sort {
      */
     @Override
     public void sort(int[] arr) {
-        //int aux 
+        int[] aux = new int[arr.length];
+        mergesort(arr, aux, 0, arr.length - 1);
+    }
+
+    private void mergesort(int[] arr, int[] aux, int lo, int hi) {
+        if (lo >= hi)
+            return;
+        int mid = (lo + hi) / 2;
+        mergesort(arr, aux, lo, mid);
+        mergesort(arr, aux, mid + 1, hi);
+        merge(arr, aux, lo, mid, hi);
     }
 
     /*
      * Merge the two sorted arrays left and right into the array arr.
      */
-    private void merge(int[] arr, int[] left, int[] right) {
-        int i = 0;
-        for (int num : left)
-            arr[i++] = num;
-        for (int num : right)
-            arr[i++] = num;
+    private void merge(int[] arr, int[] aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++)
+            aux[k] = arr[k];
+        int i = lo;
+        int j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) 
+                arr[k] = aux[j++];
+            else if (j > hi) 
+                arr[k] = aux[i++];
+            else if (aux[j] < aux[i])
+                arr[k] = aux[j++];
+            else
+                arr[k] = aux[i++];
+        }
     }
 }
